7c49e0be9347eababbc2d3a9e38076c8db7cbac9
Bhoomi <bhoomi18p2003@gmail.com>
2025-04-15 01:57:49 +0530

    Time: 0 ms (100.00%) | Memory: 56.7 MB (14.93%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,22 @@
+// Mountain array are known as Biotonic array. First increasing then decreasing
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n= arr.length;
+        int start=0;
+        int end= n-1;
+     
+        while(start<=end){
+            int mid= start+(end-start)/2;
+            if(arr[mid] > arr[mid+1]){
+                end= mid-1;
+
+            }
+            else{
+                start= mid+1;
+            }
+        }
+        return start;
+
+        
+    }
+}
