be742885a7f06a6ad0b41dee36164585227c4beb
Bhoomi <bhoomi18p2003@gmail.com>
2025-04-27 03:37:48 +0530

    Time: 6 ms (26.99%) | Memory: 41.7 MB (71.61%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -7,7 +7,7 @@ class Solution {
             while(i+1<nums.length && nums[i+1]-nums[i] == 1)
             i++;
             if(start!=nums[i]){
-                result.add(""+start+"->"+nums[i]);
+                result.add(start+"->"+nums[i]);
             }else{
                 result.add(""+start);
             }
